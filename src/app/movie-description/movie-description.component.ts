9b1cf946d442625a2b93165da2419878d6d4b06f
mhyneman8 <mallory.hyneman@gmail.com>
2021-09-19 18:11:32 -0500

    synopsis information

--- src/app/movie-description/movie-description.component.ts
+++ src/app/movie-description/movie-description.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-movie-description',
@@ -7,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDescriptionComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      title: string,
+      description: string
+    }
+     
+  ) { }
 
   ngOnInit(): void {
   }
