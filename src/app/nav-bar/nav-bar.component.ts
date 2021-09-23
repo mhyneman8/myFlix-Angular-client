0d54e59548be740924ba0cfcb9299fe2bb3554a3
mhyneman8 <mallory.hyneman@gmail.com>
2021-09-23 09:25:17 -0500

    added style

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FetchApiDataService } from '../fetch-api-data.service';
+// import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-nav-bar',
@@ -10,11 +11,20 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 export class NavBarComponent implements OnInit {
 
   constructor(
-    public fetchApiData: FetchApiDataService,
+    // public fetchApiData: FetchApiDataService,
     public router: Router,
+    public snackBar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
+  }
+
+  logout(): void {
+    localStorage.clear();
+    this.router.navigate(['welcome']);
+    this.snackBar.open("User has been logged out.", "OK", {
+      duration: 2000,
+    });
   }
   
 }
